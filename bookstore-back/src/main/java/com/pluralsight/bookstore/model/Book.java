7b169288d40af6520087726500a38718b362f7f4
Ajeet Chaulagain <chaulagainajeet@gmail.com>
2019-04-18 15:10:16 +1000

    Created DDL for creating and dropping tables

--- bookstore-back/src/main/java/com/pluralsight/bookstore/model/Book.java
+++ bookstore-back/src/main/java/com/pluralsight/bookstore/model/Book.java
@@ -1,9 +1,6 @@
 package com.pluralsight.bookstore.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
@@ -15,12 +12,23 @@ public class Book {
     @Column(length = 200)
     private String title;
 
-
+    @Column(length = 1000 )
     private String description;
+
+    @Column(name = "unit_cost")
     private Float unitCost;
+
+
     private String isbn;
+
+    @Column(name="publication_date")
+    @Temporal(TemporalType.DATE)
     private Date publicationDate;
+
+    @Column(name = "nb_of_pages")
     private Integer nbOfPages;
+
+    @Column(name="image_url")
     private String imageUrl;
     private Language language;
 
