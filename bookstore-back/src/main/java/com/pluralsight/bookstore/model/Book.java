4a715989a718cb2d2da685c80cc44d16e424a043
Ajeet Chaulagain <chaulagainajeet@gmail.com>
2019-04-18 14:04:19 +1000

    Setting up maven project directory, JPA configuration

---
+++ bookstore-back/src/main/java/com/pluralsight/bookstore/model/Book.java
@@ -0,0 +1,114 @@
+package com.pluralsight.bookstore.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Book {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(length = 200)
+    private String title;
+
+
+    private String description;
+    private Float unitCost;
+    private String isbn;
+    private Date publicationDate;
+    private Integer nbOfPages;
+    private String imageUrl;
+    private Language language;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getUnitCost() {
+        return unitCost;
+    }
+
+    public void setUnitCost(Float unitCost) {
+        this.unitCost = unitCost;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public Integer getNbOfPages() {
+        return nbOfPages;
+    }
+
+    public void setNbOfPages(Integer nbOfPages) {
+        this.nbOfPages = nbOfPages;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", unitCost=" + unitCost +
+                ", isbn='" + isbn + '\'' +
+                ", publicationDate=" + publicationDate +
+                ", nbOfPages=" + nbOfPages +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", language=" + language +
+                '}';
+    }
+}
+
